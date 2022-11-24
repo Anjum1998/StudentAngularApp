13866367248c743cc979b2cefe21e6ba0baedef1
Anju M <Anju@NTP-LAP-771.chn.nestgroup.net>
2022-11-24 15:00:52 +0530

    navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StudentEntryComponent } from './student-entry/student-entry.component';
 import { StudentViewComponent } from './student-view/student-view.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 const myRoot:Routes=[
   {
@@ -21,7 +22,8 @@ const myRoot:Routes=[
   declarations: [
     AppComponent,
     StudentEntryComponent,
-    StudentViewComponent
+    StudentViewComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
